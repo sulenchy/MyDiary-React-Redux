d6468a9b2acba26bb1e7b08f886c4929cab7efe5
sulenchy <sulaimanabiodun172@gmail.com>
2018-12-05 19:39:15 +0100

    chore(setup-redux): setup redux
    - add redux related module
    - add actions
    - add reducers
    - connect store to the app
    - add components
    
    [Finishes #162391151]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import FormContainer from './js/components/container/FormContainer'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
