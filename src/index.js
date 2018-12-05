12d1984ff6a4f4f7643e464be29c67c654f2adee
sulenchy <sulaimanabiodun172@gmail.com>
2018-12-05 19:53:50 +0100

    chore(setup-redux): setup redux
    - add redux-logger
    - add redu-devtools
    
    [Finishes #162391151]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux';
+import {createLogger} from 'redux-logger';
 import rootReducer from './reducers'
 import App from './components/App'
 
-const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = createLogger({});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 render(
   <Provider store={store}>
