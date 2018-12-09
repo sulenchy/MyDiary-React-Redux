5f59489c2a30ddcf9122e7dd0b924fef88c78d14
sulenchy <sulaimanabiodun172@gmail.com>
2018-12-09 21:14:58 +0100

    feat(view-landing-page): view landing page
    - add the background image
    - add main.css
    - add index.html
    
    [Finishes #162235696]

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibilityTodoList'
 
 const App = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
   </div>
 )
 
