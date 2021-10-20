2ef84c45495851b5f6a3269e368c0526327e3522
sulenchy <sulaimanabiodun172@gmail.com>
2021-10-20 10:02:09 +0100

    add modal component

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function Modal({
+  children, isOpen, closeFn, className, headerText
+}) {
+  if (!isOpen) return null;
+  return ReactDOM.createPortal(
+    <div id="modalBox" className={className}>
+      <div className="modal-container">
+        <header className="imgcontainer">
+          <button type="button" className="close" onClick={closeFn}>&times;</button>
+          <h2>{headerText}</h2>
+        </header>
+        {children}
+      </div>
+    </div>,
+    document.getElementById('portal')
+  );
+}
