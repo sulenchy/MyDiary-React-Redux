ca135bee8bb3b2add9896be83eae646afe170f14
sulenchy <sulaimanabiodun172@gmail.com>
2022-03-03 15:51:08 +0100

    fix(avatar-undefined): set default avatar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Icon from '../svgs';
 
 export default function Sidebar({
   payload, isSidebarShown, handleLogout
 }) {
   return ReactDOM.createPortal(
     <div id="mySidenav" className="sidenav" style={{ display: isSidebarShown ? 'block' : '' }}>
-      <a href="#" id="profile-pic"><img className="img-center" id="img-element" alt="Avatar" src={payload.user ? payload.user[0].passporturl : '#'} /></a>
-      <a id="username" href="#">{payload.user ? payload.user[0].fullname : ''}</a>
+      {
+        payload.user && payload.user[0].passporturl
+          ? <a href="#" id="profile-pic"><img className="img-center" id="img-element" alt="Avatar" src={payload.user[0].passporturl} /></a>
+          : <Icon.Avatar className="img-center" style={{ fill: '#fff' }} />
+      }
+      <a id="username" href="#">{payload.user ? payload.user[0].fullname : '' }</a>
       <hr />
       <a id="entries" href="#">Entries</a>
       <a id="profile" href="#">User Profile</a>
